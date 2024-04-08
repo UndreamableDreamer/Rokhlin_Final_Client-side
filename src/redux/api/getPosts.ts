f68b6b908b6ce021118373d49eee044927aa39f9
Pavel Rokhlin <p.rohlin@nv.dunice.net>
2024-04-08 12:06:30 +0300

    Removed unnecessary error catch.

--- src/redux/api/getPosts.ts
+++ src/redux/api/getPosts.ts
@@ -3,10 +3,6 @@ import { PostInterface } from '../../types/types';
 import api from './api';
 
 export const getPosts = async () => {
-  try {
-    const { data } = await api.get<PostInterface[]>('posts');
-    return data;
-  } catch (e: unknown) {
-    throw e;
-  }
+  const { data } = await api.get<PostInterface[]>('posts');
+  return data;
 };
